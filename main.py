24909f149901b3fc33b5d990c9d54ba97a21d143
Khan Sahib <saddamkhanashna786@gmail.com>
2022-12-24 21:58:53 +0200

    Excel Automation with python completed

---
+++ main.py
@@ -0,0 +1,44 @@
+import openpyxl as open
+
+inv_file = open.load_workbook("inventory.xlsx")
+prod_table = inv_file["Sheet1"]
+
+prod_per_suplier = {}
+total_inv_of_suplier= {}
+less_inv_prod = {}
+
+for row in range(2, prod_table.max_row+1):
+    suplier_name = prod_table.cell(row, 4).value
+    inventory = prod_table.cell(row,2).value
+    price = prod_table.cell(row,3).value
+    product_id = prod_table.cell(row, 1).value
+    inventory_price = prod_table.cell(row, 5)
+
+
+    """ Calculating number of product per supliers"""
+    if suplier_name not in prod_per_suplier:
+        prod_per_suplier[suplier_name] = 1
+    else:
+        value = prod_per_suplier[suplier_name]
+        prod_per_suplier[suplier_name] = value +1
+
+    #Calculating the total inventory value per suplier.
+    if suplier_name not in total_inv_of_suplier:
+        total_inv_of_suplier[suplier_name] = inventory * price
+    else:
+        var = total_inv_of_suplier.get(suplier_name)
+        total_inv_of_suplier[suplier_name] += inventory*price
+
+    #find the products with inventory less than 10
+    if suplier_name not in less_inv_prod and inventory< 10:
+        less_inv_prod[int(product_id)] = int(inventory)
+
+    #adding values for the inventory price
+    inventory_price.value = inventory * price
+
+
+
+print(prod_per_suplier)
+print(total_inv_of_suplier)
+print(less_inv_prod)
+inv_file.save("invetory_updated.xlsx")
